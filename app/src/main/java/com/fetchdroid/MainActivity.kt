d2378abbbf32497f20cb6d96160ec12b25219348
shad0wrider <71539127+shad0wrider@users.noreply.github.com>
2025-06-21 23:58:57 +0530

    Update MainActivity.kt

--- app/src/main/java/com/fetchdroid/MainActivity.kt
+++ app/src/main/java/com/fetchdroid/MainActivity.kt
@@ -1,8 +1,13 @@
+//Built by ❤️ https://github.com/shad0wrider
+
 package com.fetchdroid
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.app.ActivityManager
 import android.app.AlertDialog
+import android.app.NotificationManager
+import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Color
@@ -10,13 +15,14 @@ import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.provider.Settings
+import android.text.InputType
+import android.view.MotionEvent
 import android.view.WindowManager
 import android.widget.Button
 import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.edit
-
 import androidx.biometric.BiometricManager
 import androidx.biometric.BiometricPrompt
 import androidx.core.content.ContextCompat
@@ -34,10 +40,12 @@ class MainActivity : AppCompatActivity() {
 
     // Code Input
     private lateinit var codeInput: EditText
+    private lateinit var ringInput: EditText
     private lateinit var saveButton: Button
 
     private val PREFS_NAME = "TrackerPrefs"
     private val CODE_KEY = "codeWord"
+    private val RING_KEY = "ringWord"
 
 
 
@@ -78,40 +86,61 @@ class MainActivity : AppCompatActivity() {
     }
 
 
+    @SuppressLint("ClickableViewAccessibility")
     private fun showMainUI() {
+
+//        window.setFlags(
+//            WindowManager.LayoutParams.FLAG_SECURE,
+//            WindowManager.LayoutParams.FLAG_SECURE
+//        )
+
         setContentView(R.layout.activity_main)
 
         codeInput = findViewById(R.id.codeInput)
+        ringInput = findViewById<EditText>(R.id.ringInput)
         saveButton = findViewById(R.id.saveButton)
 
         val permsbutton = findViewById<Button>(R.id.whyperms)
+
+        var isPasswordVisible = false
+
+        var isRingVisible = false
 
         val sourcecode = findViewById<Button>(R.id.sourcecode)
 
         val prefs = getSharedPreferences(PREFS_NAME, MODE_PRIVATE)
         val savedCode = prefs.getString(CODE_KEY, "")
+        val ringCode = prefs.getString(RING_KEY, "")
         codeInput.setText(savedCode)
+        ringInput.setText(ringCode)
 
         //Check if App has ACCESS_BACKGROUND_LOCATION permission
         locationpermscheck()
 
-
+        //Listener for onClick Savebutton
         saveButton.setOnClickListener {
             val enteredCode = codeInput.text.toString().trim()
-            if (enteredCode == ""){
+            val enteredringCode = ringInput.text.toString().trim()
+            if (enteredCode == "" || enteredringCode == ""){
                 Toast.makeText(this,"Please Enter A CodeWord", Toast.LENGTH_SHORT).show()
             }
             else {
             prefs.edit { putString(CODE_KEY, enteredCode) }
-            Toast.makeText(this, "Code saved", Toast.LENGTH_SHORT).show()
+            prefs.edit {putString(RING_KEY, enteredringCode) }
+                codeInput.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                ringInput.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                codeInput.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.invisible_eye, 0)
+                ringInput.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.invisible_eye, 0)
+            Toast.makeText(this, "Settings Saved", Toast.LENGTH_SHORT).show()
                 }
         }
+        //Sourcecode setOnClick Listener
         sourcecode.setOnClickListener {
             val urlIntent = Intent(Intent.ACTION_VIEW,
                 "https://github.com/shad0wrider/FetchDroid".toUri())
             startActivity(urlIntent)
         }
-
+        // permsbutton setonClick Listener
         permsbutton.setOnClickListener {
             AlertDialog.Builder(this).setTitle("Why FetchDroid Needs these Permissions")
                 .setMessage("""Ability to Access Background Location and SMS are part of FetchDroid's Core Functionality
@@ -127,6 +156,59 @@ class MainActivity : AppCompatActivity() {
                 .show()
         }
 
+        //Touch Listener for Location code word password
+        codeInput.setOnTouchListener { v, event ->
+            val DRAWABLE_END = 2
+            if (event.action == MotionEvent.ACTION_UP) {
+                val drawable = codeInput.compoundDrawables[DRAWABLE_END]
+                if (drawable != null) {
+                    val bounds = drawable.bounds
+                    val x = event.rawX.toInt() - codeInput.right + codeInput.paddingEnd
+                    if (x >= -bounds.width()) {
+                        isPasswordVisible = !isPasswordVisible
+                        if (isPasswordVisible) {
+                            codeInput.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                            codeInput.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.visible_eye, 0)
+                        } else {
+                            codeInput.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                            codeInput.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.invisible_eye, 0)
+                        }
+                        // Maintain cursor at end
+                        codeInput.setSelection(codeInput.text.length)
+                        return@setOnTouchListener true
+                    }
+                }
+            }
+            false
+        }
+
+        //Touch Listener for Ring word Password
+        ringInput.setOnTouchListener { v,event ->
+            val DRAWABLE_RING_END = 2
+            if (event.action == MotionEvent.ACTION_UP){
+                val drawablering = ringInput.compoundDrawables[DRAWABLE_RING_END]
+                if (drawablering != null){
+                    val ringbounds = drawablering.bounds
+                    val ringx = event.rawX.toInt() - ringInput.right + ringInput.paddingEnd
+                    if (ringx >= -ringbounds.width()) {
+                        isRingVisible = !isRingVisible
+                        if (isRingVisible){
+                            ringInput.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+                            ringInput.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.visible_eye, 0)
+                        } else {
+                            ringInput.inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+                            ringInput.setCompoundDrawablesWithIntrinsicBounds(0, 0, R.drawable.invisible_eye, 0)
+                        }
+                        //Maintain Cursor at end
+                        ringInput.setSelection(ringInput.text.length)
+                        return@setOnTouchListener true
+                    }
+                }
+
+            }
+            false
+        }
+
 
         // Request permissions
         ActivityCompat.requestPermissions(
@@ -136,8 +218,7 @@ class MainActivity : AppCompatActivity() {
                 Manifest.permission.SEND_SMS,
                 Manifest.permission.ACCESS_FINE_LOCATION,
                 Manifest.permission.CHANGE_WIFI_STATE,
-                Manifest.permission.ACCESS_WIFI_STATE,
-                Manifest.permission.NEARBY_WIFI_DEVICES
+                Manifest.permission.ACCESS_WIFI_STATE
             ),
             1
         )
@@ -162,10 +243,11 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+
     private fun alertbox(){
         AlertDialog.Builder(this)
             .setTitle("Enable 'Allow all the time'")
-            .setMessage("To ensure FetchDroid works reliably, tap 'Permissions' > 'Location' > 'Allow all the time'.")
+            .setMessage("To ensure FetchDroid works Properly, tap 'Permissions' > 'Location' > 'Allow all the time'.")
             .setPositiveButton("Open Settings") { _, _ ->
                 val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
                     data = Uri.fromParts("package", packageName, null)
@@ -176,4 +258,3 @@ class MainActivity : AppCompatActivity() {
             .show()
     }
 }
-
