a4eace5e335ace4c5a237b2b0b5ba4880c7619ad
shad0wrider <71539127+shad0wrider@users.noreply.github.com>
2025-06-26 00:05:10 +0530

    Update SMSReceiver.kt

--- app/src/main/java/com/fetchdroid/SMSReceiver.kt
+++ app/src/main/java/com/fetchdroid/SMSReceiver.kt
@@ -1,6 +1,6 @@
 /*
  * FetchDroid - A privacy-first SMS-based phone locator
- * Copyright (C) 2025 Advait Marathe
+ * Copyright (C) 2025 Advait M
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
